69062636ea5f5a02b01d6cc17a1b8b4787d5ee6b
Agustin Gomez <agomezcampero@gmail.com>
2023-11-10 14:21:03 -0500

    allow deleting all patterns

--- src/pages/Popup/PatternSelector.jsx
+++ src/pages/Popup/PatternSelector.jsx
@@ -7,32 +7,45 @@ const PatternSelector = ({ ruleSet, onSave, currentUrl }) => {
   if (!ruleSet) return null
 
   return (
-    <div className='Patterns'>
-      {[...patterns, ''].map((pattern, index) => (
-        <div key={index} className='Pattern'>
-          {pattern ? (
-            <img
-              src={`https://www.google.com/s2/favicons?domain=${pattern}&sz=16`}
-              alt={pattern}
+    <>
+      <div className='Patterns'>
+        {[...patterns, ''].map((pattern, index) => (
+          <div key={index} className='Pattern'>
+            {pattern ? (
+              <img
+                src={`https://www.google.com/s2/favicons?domain=${pattern}&sz=16`}
+                alt={pattern}
+              />
+            ) : (
+              <span className='PatternIcon'>+</span>
+            )}
+            <input
+              className={pattern && currentUrl.includes(pattern) ? 'matchesCurrentUrl' : ''}
+              value={pattern}
+              onChange={(e) => {
+                const newPatterns = [...patterns];
+                newPatterns[index] = e.target.value;
+                setPatterns(newPatterns);
+                onSave(newPatterns.filter(Boolean));
+              }}
+              onBlur={() => onSave(patterns.filter(Boolean))}
+              placeholder={t('pattern_placeholder')}
             />
-          ) : (
-            <span className='PatternIcon'>+</span>
-          )}
-          <input
-            className={pattern && currentUrl.includes(pattern) ? 'matchesCurrentUrl' : ''}
-            value={pattern}
-            onChange={(e) => {
-              const newPatterns = [...patterns];
-              newPatterns[index] = e.target.value;
-              setPatterns(newPatterns);
-              onSave(newPatterns.filter(Boolean));
-            }}
-            onBlur={() => onSave(patterns.filter(Boolean))}
-            placeholder={t('pattern_placeholder')}
-          />
+          </div>
+        ))}
+      </div>
+      {patterns.length > 0 && (
+        <div
+          className='DeleteAllBtn'
+          onClick={() => {
+            setPatterns([]);
+            onSave([]);
+          }}
+        >
+          {t('delete_all_patterns')}
         </div>
-      ))}
-    </div>
+      )}
+    </>
   )
 }
 
